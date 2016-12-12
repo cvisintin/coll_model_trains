38a1e68c5dab5e3d05ff0ef30a64f6b6ab133a89
Casey Visintin <cvisintin@student.unimelb.edu.au>
2016-12-12 15:48:42 +1100

    revised code to calculate sunrise and sunset times for every grid cell

--- R/train_model_data.R
+++ R/train_model_data.R
@@ -2,6 +2,8 @@ require(RPostgreSQL)
 require(maptools)
 require(data.table)
 require(doMC)
+require(rgdal)
+require(sp)
 
 drv <- dbDriver("PostgreSQL")  #Specify a driver for postgreSQL type database
 con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres15", host="boab.qaeco.com", port="5432")  #Connection to database server on Boab
@@ -9,7 +11,7 @@ con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres1
 
 coll_db_hm <- as.data.table(dbGetQuery(con,"
   SELECT
-    pts.id AS pid, grid.id AS id, pts.hour AS hour, pts.day AS day, pts.month AS month, pts.year as year, pts.x AS x, pts.y AS y
+    pts.id AS pid, grid.id AS id, pts.hour AS hour, pts.day AS day, pts.month AS month, pts.year as year, ST_X(ST_Centroid(grid.geom)) AS x, ST_Y(ST_Centroid(grid.geom)) AS y
   FROM
     vline.vic_gda9455_fauna_egkcoll_train_onnetwork AS pts, gis_victoria.vic_gda9455_admin_state_1kmgrid AS grid
   WHERE
@@ -28,14 +30,54 @@ month_hist <- hist(coll_db_hm$month, breaks=0:12, xlab= 'Month', xaxt="n", xaxs=
 axis(side=1,at=month_hist$mids,labels=seq(1,12))
 dev.off()
 
-spatial <- as.matrix(cbind(x=145,y=-37))
+# spatial <- as.matrix(cbind(x=145,y=-37))
+# 
+# for (i in 1:nrow(coll_db_hm)){
+#   coll_db_hm[i,dawn:=(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dawn"),POSIXct.out=FALSE)*24)]
+#   coll_db_hm[i,dusk:=(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dusk"),POSIXct.out=FALSE)*24)]
+#   coll_db_hm[i,dawndusk:=(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dusk"),POSIXct.out=FALSE)*24)-(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dawn"),POSIXct.out=FALSE)*24)]
+# }
+# setkey(coll_db_hm,id,hour,dawndusk)
+
 
 for (i in 1:nrow(coll_db_hm)){
+  coords <- coll_db_hm[i,.(x,y)]
+  coordinates(coords) <- c("x", "y")
+  proj4string(coords) <- CRS("+init=epsg:28355")
+  spatial <- spTransform(coords, CRS("+init=epsg:4326"))
   coll_db_hm[i,dawn:=(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dawn"),POSIXct.out=FALSE)*24)]
   coll_db_hm[i,dusk:=(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dusk"),POSIXct.out=FALSE)*24)]
   coll_db_hm[i,dawndusk:=(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dusk"),POSIXct.out=FALSE)*24)-(crepuscule(spatial,as.POSIXct(paste0("2012-",month,"-15")),solarDep=6,direction=c("dawn"),POSIXct.out=FALSE)*24)]
 }
 setkey(coll_db_hm,id,hour,dawndusk)
+
+####################Sunset/sunrise variation across network#################################
+network.xy <- as.data.table(dbGetQuery(con,paste0("
+  SELECT 
+    g.id AS id, ST_X(ST_Centroid(g.geom)) AS x, ST_Y(ST_Centroid(g.geom)) AS y
+  FROM
+    vline.vic_gda9455_rail_vline_speeds AS seg,
+    gis_victoria.vic_gda9455_admin_state_1kmgrid AS g
+  WHERE
+    ST_Intersects(g.geom, seg.geom)
+  GROUP BY
+    g.id, g.geom
+    ")))
+
+for (i in 1:nrow(network.xy)){
+  coords <- network.xy[i,.(x,y)]
+  coordinates(coords) <- c("x", "y")
+  proj4string(coords) <- CRS("+init=epsg:28355")
+  spatial <- spTransform(coords, CRS("+init=epsg:4326"))
+  network.xy[i,dawn:=(crepuscule(spatial,as.POSIXct(paste0("2012-07-15")),solarDep=6,direction=c("dawn"),POSIXct.out=FALSE)*24)]
+  network.xy[i,dusk:=(crepuscule(spatial,as.POSIXct(paste0("2012-07-15")),solarDep=6,direction=c("dusk"),POSIXct.out=FALSE)*24)]
+}
+setkey(network.xy,id)
+
+range(network.xy$dawn)
+range(network.xy$dusk)
+
+#####################################################################################################
 
 registerDoMC(detectCores() - 1)
 coll_db_bgh <- foreach (i = c(0,70000,140000,210000,280000,350000,420000), .combine=rbind) %dopar% {
@@ -43,18 +85,20 @@ coll_db_bgh <- foreach (i = c(0,70000,140000,210000,280000,350000,420000), .comb
   con <- dbConnect(drv, dbname="qaeco_spatial", user="qaeco", password="Qpostgres15", host="boab.qaeco.com", port="5432")  #Connection to database server on Boab
   as.data.table(dbGetQuery(con,paste0("
   SELECT
-    y.id AS id, y.egk AS egk, AVG(y.trains) AS trains, AVG(y.length) AS length, AVG(y.speed) AS speed, y.hour AS hour, CAST(0 AS integer) AS coll
+    grid3.id AS id, grid3.egk AS egk, AVG(grid3.trains) AS trains, AVG(grid3.length) AS length, AVG(grid3.speed) AS speed, grid3.hour AS hour, CAST(0 AS integer) AS coll, grid3.x AS x, grid3.y AS y
   FROM
     (SELECT
-      x.id AS id, x.egk AS egk, COUNT(x.trains) AS trains, AVG(x.length) AS length, AVG(x.speed) AS speed, x.hour AS hour
+      grid2.id AS id, grid2.egk AS egk, COUNT(grid2.trains) AS trains, AVG(grid2.length) AS length, AVG(grid2.speed) AS speed, grid2.hour AS hour, grid2.x AS x, grid2.y AS y
     FROM
       (SELECT 
-          grid.id AS id, grid.egk AS egk, seg.dow as dow, COUNT(seg.train) AS trains, ST_Length(ST_LineMerge(ST_Union(ST_Intersection(grid.geom, seg.geom))))/1000 AS length, AVG(seg.speed) AS speed, seg.hour AS hour
+          grid.id AS id, grid.egk AS egk, seg.dow as dow, COUNT(seg.train) AS trains, ST_Length(ST_LineMerge(ST_Union(ST_Intersection(grid.geom, seg.geom))))/1000 AS length, AVG(seg.speed) AS speed, seg.hour AS hour, grid.x AS X, grid.y AS Y
         FROM
           vline.vic_gda9455_rail_vline_speeds AS seg, 
           (SELECT 
             g.id as id,
             ST_Value(p.rast,ST_Centroid(g.geom)) AS egk,
+            ST_X(ST_Centroid(g.geom)) AS x,
+            ST_Y(ST_Centroid(g.geom)) AS y,
             g.geom as geom
           FROM 
             gis_victoria.vic_gda9455_grid_egk_preds_brt AS p,
@@ -76,12 +120,12 @@ coll_db_bgh <- foreach (i = c(0,70000,140000,210000,280000,350000,420000), .comb
         AND
           seg.speed <= 160
         GROUP BY
-          grid.id, grid.egk, seg.train, seg.hour, seg.dow
-        ) as x
+          grid.id, grid.egk, seg.train, seg.hour, seg.dow, grid.x, grid.y
+        ) as grid2
     GROUP BY
-      x.id, x.egk, x.hour, x.dow) as y
+      grid2.id, grid2.egk, grid2.hour, grid2.dow, grid2.x, grid2.y) as grid3
    GROUP BY
-    y.id, y.egk, y.hour
+    grid3.id, grid3.egk, grid3.hour, grid3.x, grid3.y
     ")))
 }
 setkey(coll_db_bgh,id,hour)
@@ -91,6 +135,10 @@ setkey(coll_db_bgh,id,hour)
 
 registerDoMC(detectCores() - 1)
 coll_db_bghm <- foreach (i = 1:12, .combine=rbind) %dopar% {
+  coords <- coll_db_bgh[,.(x,y)]
+  coordinates(coords) <- c("x", "y")
+  proj4string(coords) <- CRS("+init=epsg:28355")
+  spatial <- spTransform(coords, CRS("+init=epsg:4326"))
   cbind(coll_db_bgh,
     "dawn"=(crepuscule(spatial,as.POSIXct(paste0("2012-",as.integer(sprintf("%02d",i)),"-15")),solarDep=6,direction=c("dawn"),POSIXct.out=FALSE)*24),
     "dusk"=(crepuscule(spatial,as.POSIXct(paste0("2012-",as.integer(sprintf("%02d",i)),"-15")),solarDep=6,direction=c("dusk"),POSIXct.out=FALSE)*24),
